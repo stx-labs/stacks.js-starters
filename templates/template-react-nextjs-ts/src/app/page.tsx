0c4fa42631e39f398dc3a9c006eada51fc00d160
janniks <janniks@users.noreply.github.com>
2025-06-03 15:21:40 +0800

    fix: update react usage

--- templates/template-react-nextjs-ts/src/app/page.tsx
+++ templates/template-react-nextjs-ts/src/app/page.tsx
@@ -4,9 +4,7 @@ import Image from "next/image";
 import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
-import { Connect } from "@stacks/connect-react";
-
-import ConnectWallet, { userSession } from "../components/ConnectWallet";
+import ConnectWallet from "../components/ConnectWallet";
 import ContractCallVote from "../components/ContractCallVote";
 
 export default function Home() {
@@ -19,138 +17,122 @@ export default function Home() {
   if (!isClient) return null;
 
   return (
-    <Connect
-      authOptions={{
-        appDetails: {
-          name: "Stacks Next.js Template",
-          icon: window.location.origin + "/logo.png",
-        },
-        redirectTo: "/",
-        onFinish: () => {
-          window.location.reload();
-        },
-        userSession,
-      }}
-    >
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/app/page.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <h2>Next.js + Stacks.js 👋</h2>
-        </div>
-
+    <main className={styles.main}>
+      <div className={styles.description}>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>src/app/page.tsx</code>
+        </p>
         <div>
-          {/* ConnectWallet file: `./src/components/ConnectWallet.js` */}
-          <ConnectWallet />
-
-          {/* ContractCallVote file: `./src/components/ContractCallVote.js` */}
-          <ContractCallVote />
-        </div>
-
-        <div className={styles.grid}>
-          <br />
-
           <a
-            href="https://docs.hiro.so/"
-            className={styles.card}
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2>
-              Learn Stacks <span>→</span>
-            </h2>
-          </a>
-
-          <a
-            href="https://docs.hiro.so/stacks.js/overview"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn to Build on Stacks <span>→</span>
-            </h2>
-          </a>
-
-          <br />
-          <hr />
-          <br />
-
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>→</span>
-            </h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>→</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>→</span>
-            </h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>→</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL with Vercel.
-            </p>
+            By{" "}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
           </a>
         </div>
-      </main>
-    </Connect>
+      </div>
+
+      <div className={styles.center}>
+        <h2>Next.js + Stacks.js 👋</h2>
+      </div>
+
+      <div>
+        {/* ConnectWallet file: `./src/components/ConnectWallet.js` */}
+        <ConnectWallet />
+
+        {/* ContractCallVote file: `./src/components/ContractCallVote.js` */}
+        <ContractCallVote />
+      </div>
+
+      <div className={styles.grid}>
+        <br />
+
+        <a
+          href="https://docs.hiro.so/"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn Stacks <span>→</span>
+          </h2>
+        </a>
+
+        <a
+          href="https://docs.hiro.so/stacks.js/overview"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn to Build on Stacks <span>→</span>
+          </h2>
+        </a>
+
+        <br />
+        <hr />
+        <br />
+
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Docs <span>→</span>
+          </h2>
+          <p>Find in-depth information about Next.js features and API.</p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Learn <span>→</span>
+          </h2>
+          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Templates <span>→</span>
+          </h2>
+          <p>Explore the Next.js 13 playground.</p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+          className={styles.card}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2>
+            Deploy <span>→</span>
+          </h2>
+          <p>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
   );
 }
